fd1a02e34e03a0f000262751bd8da2304061a985
Mustafa Mahmoud <mmh.ustafa9@gmail.com>
2025-07-29 19:30:13 +0300

    products

--- ecommecre-ui/src/app/app.routes.ts
+++ ecommecre-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductPage } from './pages/product/product-page/product-page';
+import { ProductDetails } from './pages/product/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProductPage },
+    { path: 'product/:id', component: ProductDetails }
+];
--- ecommecre-ui/src/app/app.ts
+++ ecommecre-ui/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductPage } from './pages/product/product-page/product-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,ProductPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
